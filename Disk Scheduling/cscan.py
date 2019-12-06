9f037870ba8a4fa845c75bdd34b9129a7d71fdfb
Vraj Gohil <vrajhil3999@gmail.com>
2019-12-06 11:47:04 +0000

    added cscan

---
+++ Disk Scheduling/cscan.py
@@ -0,0 +1,19 @@
+requestSequence = [int(i) for i in input("Enter the sequence :").split()]
+headPostion = int(input("Enter the initial head postion :"))
+requestSequence.sort()
+lessThenHead = [0]
+greaterThenHead = []
+for i in requestSequence:
+    if i < headPostion:
+        lessThenHead.append(i)
+    else:
+        greaterThenHead.append(i)
+greaterThenHead.append(199)
+seekSequence = greaterThenHead + lessThenHead
+print("Seek Sequence is :", seekSequence)
+totalSeek = seekSequence[0]-headPostion
+print(seekSequence[0],'-',headPostion)
+for i in range (1,len(seekSequence)):
+    print(seekSequence[i],'-',seekSequence[i-1])
+    totalSeek += (seekSequence[i]-seekSequence[i-1])
+print("Total number of seek operations = ", totalSeek+199)
